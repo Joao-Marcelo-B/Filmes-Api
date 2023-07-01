59ece8191025d575db7de3db41ffcd6b0964b67a
João Marcelo Batista Narciso <joaomarcelobatistanarciso@gmail.com>
2023-07-01 17:42:38 -0300

    Atualizando FilmeController adicionando o verbo DELETE para remoção de tuplas do Banco de Dados

--- FilmesApi/FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/FilmesApi/Controllers/FilmeController.cs
@@ -30,9 +30,9 @@ public class FilmeController : ControllerBase
     }
 
     [HttpGet]
-    public IEnumerable<Filme> RecuperaFilmes([FromQuery]int skip = 0, [FromQuery]int take = 10)
+    public IEnumerable<ReadFilmeDto> RecuperaFilmes([FromQuery]int skip = 0, [FromQuery]int take = 10)
     {
-        return _context.Filmes.Skip(skip).Take(take);
+        return _mapper.Map<List<ReadFilmeDto>>(_context.Filmes.Skip(skip).Take(take));
     }
 
     [HttpGet("{id}")]
@@ -42,7 +42,9 @@ public class FilmeController : ControllerBase
 
         if (filme == null) return NotFound();
 
-        return Ok(filme);
+        var filmeDto = _mapper.Map<ReadFilmeDto>(filme);
+
+        return Ok(filmeDto);
     }
 
     [HttpPut("{id}")]
@@ -83,6 +85,11 @@ public class FilmeController : ControllerBase
     [HttpDelete("{id}")]
     public IActionResult DeletaFilme(int id)
     {
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null) return NotFound();
 
+        _context.Filmes.Remove(filme);
+        _context.SaveChanges();
+        return NoContent();
     }
 }
