9acfa9f79f852c571175906defb9cb6f2873a967
João Marcelo Batista Narciso <joaomarcelobatistanarciso@gmail.com>
2023-07-16 23:48:53 -0300

    Update

--- FilmesApi/FilmesApi/Data/Dtos/ReadFilmeDto.cs
+++ FilmesApi/FilmesApi/Data/Dtos/ReadFilmeDto.cs
@@ -4,6 +4,7 @@ namespace FilmesApi.Data.Dtos;
 
 public class ReadFilmeDto
 {
+    public string Id { get; set; }
     public string Titulo { get; set; }
     public string Genero { get; set; }
     public int Duracao { get; set; }
